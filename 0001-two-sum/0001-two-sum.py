547f1baf37d7d16b23eab81a076872bb5e469db8
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-05-31 03:00:03 +0900

    Time: 0 ms (100%), Space: 20.6 MB (19.06%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # using dictionary
+        seen = {}
+        for index, value in enumerate(nums):
+            diff = target - value
+            if diff not in seen:
+                seen[value] = index
+            else:
+                return [seen[diff], index]
