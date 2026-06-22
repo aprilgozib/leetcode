e689ffe8ba225b5c1af6a9f543533eb9bb98243c
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-06-22 23:37:52 +0900

    Time: 5 ms (11.18%), Space: 19.3 MB (23.65%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        # using dictionary
+        seen = {}
+        for ch in s:
+            seen[ch] = seen.get(ch, 0) + 1
+        
+        for ch in t:
+            if ch in seen and seen[ch] > 0:
+                seen[ch] -= 1
+            else:
+                return ch
