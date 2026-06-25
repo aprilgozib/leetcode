25cc0ef0b20281e99336f52bceeb2b573e653c40
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-06-26 05:11:37 +0900

    Time: 43 ms (33.91%), Space: 31.5 MB (43.89%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # if the sum is -, restart
+        # if the sum - occur -> restart
         curr_sum = nums[0]
         max_sum = nums[0]
         for i in nums[1:]:
-            curr_sum = max(curr_sum + i, i) # if -, restart
+            curr_sum = max(curr_sum + i, i) # if - occur restart
             max_sum = max(max_sum, curr_sum)
-
         return max_sum
