97142d4b4303624b64f2f691528223c236a4045c
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-06-26 05:14:31 +0900

    Time: 0 ms (100%), Space: 19.3 MB (55.98%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,8 +1,11 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits) -1, -1, -1):
+        # start at the back
+        # if 9 -> 0 and plus 1 at the end
+        for i in range(len(digits) - 1, -1, -1):
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
-            digits[i] = 0
+            else: #digits[i] == 9
+                digits[i] = 0
         return [1] + digits
