85d8a8e48ade68e569c801100a67869d4ec1afba
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-06-27 00:39:12 +0900

    Time: 0 ms (100%), Space: 19.3 MB (37.77%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,15 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # using slicing
-        # nums1[:] = sorted(nums1[:m] + nums2)
-        
-        # using two pointers -> from the back
-        p1 = m - 1 # 2
-        p2 = n - 1 # 2
-        p = m + n - 1 # 5
-
-        while p1 >= 0 and p2 >= 0: # still element to compare
+        # use pointer
+        # from the back
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+        while p1 >= 0 and p2 >= 0:
             if nums1[p1] < nums2[p2]:
                 nums1[p] = nums2[p2]
                 p2 -= 1
@@ -19,5 +16,4 @@ class Solution:
                 nums1[p] = nums1[p1]
                 p1 -= 1
             p -= 1
-
-        nums1[:p2 + 1] = nums2[:p2 + 1]
+        nums1[:p2 + 1] = nums2[:p2 + 1] # if numbers left at nums2
