8f1b4c2dfd9962b524cec69d01a7f98fe852e838
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-06-27 02:01:57 +0900

    Time: 51 ms (52.49%), Space: 28.7 MB (41.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0]
+        # max_profit = toady - min_price
+        min_price = float('inf')
         max_profit = 0
         for today in prices:
             min_price = min(min_price, today)
