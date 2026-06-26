d171bab478ec3740af6d6030843f458896babecb
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-06-27 02:54:17 +0900

    Time: 4 ms (41.26%), Space: 21.1 MB (42.99%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,16 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # using dict -> O(n) space complexity
-        #seen = {}
-        #for i in nums:
-        #    seen[i] = seen.get(i, 0) + 1
-        
-        #for key, value in seen.items():
-        #    if value == 1:
-        #        return key
-
-        # using xor -> space complexity O(1)
-        res = 0
+        sum = 0
         for i in nums:
-            res ^= i
-        return res
+            sum ^= i
+        return sum
