8d2a19c7d50641839d2485e75fb36699b6ab79b8
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-06-28 01:18:15 +0900

    Time: 0 ms (100%), Space: 26.6 MB (43.85%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,6 +4,5 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # slicing
-        # [1,2] k = 7 <- we need k = k % len(nums)
         k = k % len(nums)
         nums[:] = nums[-k:] + nums[:-k]
