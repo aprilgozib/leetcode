2a0e27796ded26884f96b70145af0d0a2be74ab6
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-06-28 01:51:41 +0900

    Time: 1902 ms (25.73%), Space: 58.2 MB (23.06%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -3,15 +3,14 @@ class Solution:
         # F, T list checking
         if n < 2:
             return 0
-        
-        is_prime = [True] * n # [T, T, T, T, T...]
+
+        is_prime = [True] * n
         is_prime[0] = False
-        is_prime[1] = False # [F, F, T, T, T, T...]
+        is_prime[1] = False
 
         for i in range(2, n):
-            if is_prime[i]: # True
-                for j in range(i*i, n, i): # i*i is point
+            if is_prime[i]:
+                for j in range(i*i, n, i):
                     is_prime[j] = False
-        
         return sum(is_prime)
-
+        
