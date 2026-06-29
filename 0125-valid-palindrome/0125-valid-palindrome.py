e00f2a8c67d7baf4894a5cf079704472394cfc31
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-06-30 05:15:19 +0900

    Time: 9 ms (41.87%), Space: 23.4 MB (8.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,16 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # use isalnum, two pointer 
+        # use isalnum, two pointer
         new = []
         for ch in s:
             if ch.isalnum():
                 new.append(ch.lower())
-        
-        left = 0
-        right = len(new) - 1
+        left, right = 0, len(new) - 1
         while left < right:
-            if new[left] != new[right]:
+            if new[left] == new[right]:
+                left += 1
+                right -= 1
+            else:
                 return False
-            left += 1
-            right -= 1
         return True
