1023d3a4fce62ad056493455e5d2391a229fe014
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-07-01 03:51:10 +0900

    Time: 6 ms (45.17%), Space: 20.4 MB (88.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,22 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # swith
-        if len(nums) < 2:
+        if len(nums) < 1:
             return nums
         # two pointers
         left, right = 0, 1
         while right < len(nums):
             if nums[left] == 0:
-                if nums[right] == 0: # 0, 0
-                    right += 1
-                else: # 0, 1 -> 1, 0
+                if nums[right] != 0:
                     nums[left], nums[right] = nums[right], nums[left]
                     left += 1
                     right += 1
-            else: # 1,
+                else: # nums[right] == 0:
+                    right += 1
+            else: # nums[left] != 0
                 left += 1
                 right += 1
-            
-
-            
