45be77859f209fc9c5186921e4bd078cb7ab95a6
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-07-01 03:52:58 +0900

    Time: 7 ms (10.23%), Space: 23.4 MB (43.86%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,12 +3,8 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        # use slicing
-        # s[:] = s[::-1]
-
-        # use two pointer
-        left = 0
-        right = len(s) - 1
+        # use two pointers
+        left, right = 0, len(s) - 1
         while left < right:
             s[left], s[right] = s[right], s[left]
             left += 1
