0b7c9704f166727d93d88518e6ffd9999656f35f
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-07-02 03:33:54 +0900

    Time: 0 ms (100%), Space: 19.1 MB (89.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,5 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # use prev2, prev1 -> prev1 compare prev2 + num
         if len(nums) == 1:
             return nums[0]
 
@@ -8,5 +9,4 @@ class Solution:
             curr = max(prev1, prev2 + num)
             prev2 = prev1
             prev1 = curr
-        
         return curr
