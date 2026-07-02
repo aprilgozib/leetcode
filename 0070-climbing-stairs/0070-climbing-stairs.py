bcfb83fbaf54573bf4679382d9debfa08dca1458
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-07-03 01:45:11 +0900

    Time: 0 ms (100%), Space: 19.1 MB (85.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -9,10 +9,9 @@ class Solution:
         if n <= 2:
             return n
         
-        prev1, prev2 = 2, 1
+        prev2, prev1 = 1, 2
         for i in range(3, n+1):
-            curr = prev1 + prev2
+            curr = prev2 + prev1
             prev2 = prev1
             prev1 = curr
-            
         return prev1
