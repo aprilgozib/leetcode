0b3303816145404b948125f02a5ed474804aa75c
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-07-04 01:57:06 +0900

    Time: 54 ms (55.86%), Space: 22.6 MB (54.28%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,7 +6,7 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        # use slow, fast two pointers
+        # slow, fast two pointers
         slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
