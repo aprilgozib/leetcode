a3d2ea7dfa9c495377fbb0fb7d0174b3d1d81526
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-07-05 01:43:22 +0900

    Time: 4 ms (26.38%), Space: 19.4 MB (11.47%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,24 +1,12 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # dictionary -> enumerate, items(), values()
-        # set -> & intersection
-        # two pointers -> move left to right
-        nums1.sort()
-        nums2.sort()
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        seen = {}
         res = []
-        left, right = 0, 0
-        while left < len(nums1) and right < len(nums2):
-            if nums1[left] == nums2[right]:
-                res.append(nums1[left])
-                left += 1
-                right += 1
-                # [1,1,2,2] duplicate
-                while left < len(nums1) and nums1[left] == nums1[left - 1]:
-                    left += 1
-                while right < len(nums2) and nums2[right] == nums2[right - 1]:
-                    right += 1
-            elif nums1[left] < nums2[right]:
-                left += 1
-            else:
-                right += 1
+        for num in nums1:
+            seen[num] = seen.get(num, 0) + 1
+        for num in nums2:
+            if num in seen:
+                res.append(num)
         return res
