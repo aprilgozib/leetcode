36c2a0c1dd2bc784fb539c136ed2b19db0682f72
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-07-08 00:38:49 +0900

    Time: 0 ms (100%), Space: 19.5 MB (39.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,20 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # use two pointer, binary search
-        # focus on the sorted part
+        # binary search
+        # left, right sorted part
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
             elif nums[left] <= nums[mid]: # left sorted
-                if nums[left] <= target <= nums[mid]: # left part
+                if nums[left] <= target <= nums[mid]: # target at left
                     right = mid - 1
-                else: # right part
+                else: # target at right
                     left = mid + 1
             else: # right sorted
-                if nums[mid] <= target <= nums[right]: # right part
+                if nums[mid] <= target <= nums[right]: # target at right
                     left = mid + 1
-                else: # left part
+                else:
                     right = mid - 1
         return -1
