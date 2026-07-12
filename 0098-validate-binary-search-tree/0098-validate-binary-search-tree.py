1a1481cf9ea2725dd1fc6434232c29f647a5895d
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-07-13 04:34:31 +0900

    Time: 0 ms (100%), Space: 21 MB (21.38%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -10,11 +10,10 @@ class Solution:
         def dfs(node, low, high):
             if not node:
                 return True
-
+            
             if node.val <= low or node.val >= high:
                 return False
-
+            
             return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
 
-        return dfs(root, float("-inf"), float("inf"))
-        
+        return dfs(root, float('-inf'), float('inf'))
