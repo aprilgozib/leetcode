ca707f67d5ee7f77ef0ea7221166d18485f40329
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-07-31 09:59:18 +0900

    Time: 27 ms (83.11%), Space: 31.3 MB (94.1%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,7 +3,9 @@ class Solution:
         # if minus -> reset
         curr_sum = nums[0]
         max_sum = nums[0]
+
         for i in nums[1:]:
-            curr_sum = max(curr_sum + i, i)
+            curr_sum = max(curr_sum + i, i) # i is bigger if minus occur
             max_sum = max(max_sum, curr_sum)
+
         return max_sum
