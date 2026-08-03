918ae0b81bc06181f3940c5602cb933463553c1e
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-03 15:06:26 +0900

    Time: 0 ms (100%), Space: 20.5 MB (36.47%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        seen = {}
+        seen  = {}
         for i in range(len(numbers)):
             diff = target - numbers[i]
             if diff not in seen:
