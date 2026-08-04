008be058c2597b2d2df9edc8a627ea48ef377f81
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-04 23:24:43 +0900

    Time: 8 ms (22.98%), Space: 20.4 MB (61.81%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,7 +5,7 @@ class Solution:
         """
         if len(nums) < 1:
             return nums
-        # two pointers
+
         left, right = 0, 1
         while right < len(nums):
             if nums[left] == 0:
@@ -13,8 +13,9 @@ class Solution:
                     nums[left], nums[right] = nums[right], nums[left]
                     left += 1
                     right += 1
-                else: # nums[right] == 0:
+                else: # nums[right] == 0
                     right += 1
             else: # nums[left] != 0
                 left += 1
                 right += 1
+        
