bddb7ac412d559c8eef22cf272d4e495578dd32d
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-05 19:05:10 +0900

    Time: 0 ms (100%), Space: 19.8 MB (64.95%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -5,6 +5,9 @@ class Solution:
         t_max = float('-inf')
 
         for num in nums:
+            if num == f_max or num == s_max or num == t_max:
+                continue
+
             if num > f_max:
                 t_max = s_max
                 s_max = f_max
@@ -14,8 +17,8 @@ class Solution:
                 s_max = num
             elif num < s_max and num > t_max:
                 t_max = num
+
         if t_max == float('-inf'):
             return f_max
         else:
             return t_max
-                
