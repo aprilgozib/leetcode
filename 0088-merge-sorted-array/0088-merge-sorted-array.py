504af3f9df32115fd152258283661288681c9c55
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-05 20:09:25 +0900

    Time: 0 ms (100%), Space: 19.5 MB (9.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,8 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # compare from the back
-        p1, p2, p = m - 1, n - 1, m + n - 1
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+
         while p1 >= 0 and p2 >= 0:
             if nums1[p1] < nums2[p2]:
                 nums1[p] = nums2[p2]
