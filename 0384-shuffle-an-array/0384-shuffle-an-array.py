07fb852234f815abe13729496b7128371ee1f86d
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-05 18:59:57 +0900

    Time: 19 ms (68.28%), Space: 22.7 MB (23.14%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.py
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -5,13 +5,10 @@ class Solution:
         self.nums = nums
 
     def reset(self) -> List[int]:
-        # return og
         self.nums = self.og[:]
         return self.nums
 
     def shuffle(self) -> List[int]:
-        # randon shuffle
-        # back to front
         for i in range(len(self.nums) - 1, 0, -1):
             j = random.randint(0, i)
             self.nums[i], self.nums[j] = self.nums[j], self.nums[i]
