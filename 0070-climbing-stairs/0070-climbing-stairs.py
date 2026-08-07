f845f55fe01c05a7140759bcde38f8a384c66ec4
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-07 22:31:34 +0900

    Time: 0 ms (100%), Space: 19.2 MB (52.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,17 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # fibonachi
-        # n = 1 -> 1
-        # n = 2 -> 1,1 2
-        # n = 3 -> 1,1,1 1,2 2,1 
-        # n = 4 -> 1,1,1,1 1,1,2 1,2,1 2,1,1 2,2
+        # 1 -> 1
+        # 2 -> 1+1, 2
+        # 3 -> 1+1+1, 1+2, 2+1
+        # 4 -> 1+1+1+1, 1+1+2, 1+2+1, 2+1+1, 2+2
         # f(n) = f(n-2) + f(n-1)
+
         if n <= 2:
             return n
-        
+
         prev2, prev1 = 1, 2
         for i in range(3, n+1):
             curr = prev2 + prev1
             prev2 = prev1
             prev1 = curr
-        return prev1
+    
+        return curr
+        
