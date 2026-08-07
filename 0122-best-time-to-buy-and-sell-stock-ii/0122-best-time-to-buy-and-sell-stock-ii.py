9bff09f5998f2f9bdce0da7e30971e8839e78cbd
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-07 22:38:00 +0900

    Time: 0 ms (100%), Space: 20.4 MB (52.3%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,7 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        total = 0
+        # yesterday < today -> total += today - yesterday
         yesterday = prices[0]
+        total = 0
+
         for today in prices[1:]:
             if today > yesterday:
                 total += today - yesterday
