bae69ed8c15596888e2027ab745afbf40570c048
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-08 23:20:31 +0900

    Time: 0 ms (100%), Space: 19.3 MB (66.79%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,18 +1,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        # add all even, odd - 1
-        # if odd + 1
+        # count characters
         seen = {}
-        is_odd = False
         for ch in s:
             seen[ch] = seen.get(ch, 0) + 1
+        # {a:1, b:1, c:4, d:2}
+        # add all even, odd - 1
+        # if odd is there + 1
         res = 0
+        is_odd = False
         for count in seen.values():
             res += count // 2 * 2
-            if count % 2 == 1: #odd
+            if count % 2 == 1: # odd
                 is_odd = True
-            
+        
         if is_odd:
             return res + 1
         else:
             return res
+        
