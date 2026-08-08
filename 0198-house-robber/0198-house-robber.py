ea9c2934fc8ef46095b1119985067a48811d8a96
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-08 23:12:34 +0900

    Time: 0 ms (100%), Space: 19.2 MB (61.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # use prev2, prev1 -> prev1 compare prev2 + curr
+        # compare prev2 + curr vs prev1
         if len(nums) == 1:
             return nums[0]
 
         prev2, prev1 = 0, 0
         for num in nums:
-            curr = max(prev1, prev2 + num)
+            curr = max(prev2 + num, prev1)
             prev2 = prev1
             prev1 = curr
+
         return curr
