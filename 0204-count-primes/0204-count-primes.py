44d0fb467fa6aed27c3183024490f91e1beae087
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-10 20:19:27 +0900

    Time: 5136 ms (5.86%), Space: 58 MB (69.69%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,7 +1,5 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        # T, F
-        # i*i, n, i
         if n < 2:
             return 0
 
@@ -12,5 +10,5 @@ class Solution:
         for i in range(2, n):
             for j in range(i * i, n, i):
                 is_prime[j] = False
-        
+
         return sum(is_prime)
