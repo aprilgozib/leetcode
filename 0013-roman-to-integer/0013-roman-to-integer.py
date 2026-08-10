20071b5e0a79ad6ec37532672512e7a9b12071e5
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-10 20:12:45 +0900

    Time: 1 ms (87.48%), Space: 19.3 MB (19.36%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -3,9 +3,10 @@ class Solution:
         seen = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         res = 0
         for i in range(len(s) - 1):
-            if seen[s[i]] < seen[s[i+1]]:
+            if seen[s[i]] < seen[s[i + 1]]:
                 res -= seen[s[i]]
             else:
                 res += seen[s[i]]
         res += seen[s[-1]]
         return res
+            
