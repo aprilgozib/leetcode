7f305207e6eaf64923f52d8ef6e0f842009a8388
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-10 20:10:03 +0900

    Time: 41 ms (84.04%), Space: 19.3 MB (31.6%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -9,13 +9,13 @@ class Solution:
         x = abs(x)
 
         res = 0
-        INT_MAX = 2**31 - 1
-        INT_MIN = -2**31 - 1
-        
+        INT_MAX = 2 ** 31 - 1
+        INT_MIN = -2 ** 31 - 1
+
         while x > 0:
-            digit = x % 10 # 3 -> 2
-            res = res * 10 + digit # 0 * 10 + 3 -> 3 * 10 + 2
-            x //= 10 # 12 -> 1
+            digit = x % 10 # 3 -> 2 -> 1
+            res = res * 10 + digit # 0 * 10 + 3 -> 3 * 10 + 2 -> 32 * 10 + 1
+            x //= 10 # 12 -> 1 -> 0
 
         res *= sign
         if res < INT_MIN or res > INT_MAX:
