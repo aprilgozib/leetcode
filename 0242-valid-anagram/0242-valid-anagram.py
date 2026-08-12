440199adf7880f370695d4feadc161dd91e125c9
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-12 21:43:07 +0900

    Time: 15 ms (44.53%), Space: 19.5 MB (46.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
         seen = {}
         for ch in s:
             seen[ch] = seen.get(ch, 0) + 1
+
         for ch in t:
             if ch not in seen:
                 return False
+            elif seen[ch] < 0:
+                return False
             else:
                 seen[ch] -= 1
-                
-        for value in seen.values():
-            if value > 0:
-                return False
-            elif value < 0:
+            
+        for count in seen.values():
+            if count > 0:
                 return False
 
         return True
