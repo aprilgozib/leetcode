cc1bff315adf69f32314c8c62ac1df40347152f4
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-16 21:05:36 +0900

    Time: 56 ms (6.38%), Space: 37.2 MB (11.57%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -3,7 +3,7 @@ class Solution:
         seen = {}
         for num in nums:
             seen[num] = seen.get(num, 0) + 1
-        
+
         for count in seen.values():
             if count > 1:
                 return True
