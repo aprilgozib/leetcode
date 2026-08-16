630ef8fd72646404aba0d0db8737cc8f9c87ee5a
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-16 23:33:45 +0900

    Time: 0 ms (100%), Space: 19.5 MB (12.15%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -4,7 +4,7 @@ class Solution:
         for num in nums1:
             if num not in seen:
                 seen[num] = 1
-        # seen = {1:1, 2:1}
+
         res = []
         for num in nums2:
             if num in seen and seen[num] > 0:
