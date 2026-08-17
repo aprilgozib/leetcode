1ce3c1573691afecd01a804a86e375b8adea0212
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-17 17:16:17 +0900

    Time: 1 ms (70.03%), Space: 21.1 MB (43.32%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        seen = {} #{2:2, 1:1}
+        res = 0
         for num in nums:
-            seen[num] = seen.get(num, 0) + 1
-        for value, count in seen.items():
-            if count == 1:
-                return value
+            res ^= num
+
+        return res
