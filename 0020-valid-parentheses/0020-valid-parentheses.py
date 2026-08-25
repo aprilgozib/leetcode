b8c205868097dc939d04a9f5d9315aae2dd84ae0
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-25 23:14:27 +0900

    Time: 0 ms (100%), Space: 19.3 MB (63.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,13 +3,14 @@ class Solution:
         seen = {'(':')', '{':'}', '[':']'}
         stack = []
         for ch in s:
-            if ch in seen:
+            if ch in seen: # open
                 stack.append(ch)
-            elif ch in seen.values():
-                if not stack:
+            elif ch in seen.values(): # close
+                if not stack: # empty
                     return False
-                elif ch == seen[stack[-1]]:
+                elif ch == seen[stack[-1]]: # match
                     stack.pop()
-                else:
+                else: # not match
                     return False
-        return not stack
+
+        return not stack # stack should be empty
