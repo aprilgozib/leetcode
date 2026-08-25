4bf92dcb6ea309936d5815413ec3c78018e541b8
NaHyung Kwon <46991725+aprilgozib@users.noreply.github.com>
2026-08-25 22:41:37 +0900

    Time: 70 ms (61.01%), Space: 19.7 MB (23.31%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -3,9 +3,7 @@ class Solution:
         seen = {}
         for ch in s:
             seen[ch] = seen.get(ch, 0) + 1
-        
         for i in range(len(s)):
             if seen[s[i]] == 1:
                 return i
-
         return -1
